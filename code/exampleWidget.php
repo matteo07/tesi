f200a2e97099b4c045f48e90d571e09d52fd86a6
dema <a@.it>
2017-08-18 16:59:49 +0200

    include codice php

---
+++ code/exampleWidget.php
@@ -0,0 +1,25 @@
+<?php
+class simpleWidget extends WP_Widget {
+    function __construct() {
+        parent::__construct( false, 'Kirivo - Simple widget' );
+    }
+
+    function widget( $args, $instance ) { ?>
+        <div>
+            <h1>Titolo del widget : <?php echo $instance['title']?></h1>
+        </div>
+        <?php
+    }
+
+    function form( $instance ) {?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>"
+            'Titolo:');</label>
+            <input class="widefat" id="<?php 
+            echo $this->get_field_id('title');?>" 
+            name="<?php echo $this->get_field_name( 'title' ); ?>" 
+            type="text" value="<?php echo $instance['title']; ?>" />
+        </p>
+        <?php
+    }
+}
---
+++ code/intializeWidget.php
@@ -0,0 +1,33 @@
+<?php
+function getKirivoWidgets() {
+    $res = array();
+    foreach(glob( dirname(_FILE_)."/widget-kirivo/*Widget.php") 
+            as $filename) {
+        array_push($res,$filename);
+    };
+    return $res;
+}
+
+function getOriginiWidgets() {
+    $res = array();
+    foreach(glob( dirname(_FILE_)."/widget-origini/*Widget.php") 
+            as $filename) {
+        array_push($res,$filename);
+    };
+    return $res;
+}
+
+function my_register_widgets() {
+    foreach(getKirivoWidgets() as $filename) {
+        require_once $filename;
+        register_widget(str_replace('.php','',str_replace(dirname(_FILE_).
+            '/widget-kirivo/','',$filename)));
+    }
+    foreach(getOriginiWidgets() as $filename) {
+        require_once $filename;
+        register_widget(str_replace('.php','',str_replace(dirname(_FILE_).
+            '/widget-origini/','',$filename)));
+    };
+}
+ 
+add_action( 'widgets_init', 'my_register_widgets' );
